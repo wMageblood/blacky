ce3f8ddd78823b3db896c529a3b5d7908d052ed2
wMageblood <enzo.luciano@outlook.es>
2026-06-05 16:34:50 -0600

    fixed routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,5 @@
 const ROUTES = [
   { path: "/", routeName: "home" },
-  { path: "/servicios", routeName: "servicios" },
-  { path: "/contactanos", routeName: "contactanos" },
 ];
 
 export default ROUTES;
