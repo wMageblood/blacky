949cf10b29e5848ade195a79816f99bb7a0263ef
wMageblood <enzo.luciano@outlook.es>
2026-06-06 19:15:24 -0600

    fixed some stuff

--- src/Pages/Sections/Services.tsx
+++ src/Pages/Sections/Services.tsx
@@ -2,6 +2,11 @@ import { ReactCompareSlider } from "react-compare-slider";
 import { ServicesDetailed } from "../../components/ServicesDetailed.tsx"
 import biggieBefore from "../../assets/biggie-before.png"
 import biggieAfter from "../../assets/biggie-after.png"
+import robertBefore from "../../assets/robertBefore.png"
+import robertAfter from "../../assets/robertAfter.png"
+import roboAfter from "../../assets/roboAfter.png"
+import roboBefore from "../../assets/roboBefore.png"
+import beforeAfter from "../../assets/beforeAfter.png"
 
 export const Services = () => {
   return (
@@ -9,13 +14,17 @@ export const Services = () => {
       <div className='mx-auto w-[85%] md:w-[80%]'>
         <ServicesDetailed />
       </div>
-        <h1 className='text-3xl tracking-tighter font-semibold text-center mt-15'>Antes y después</h1>
+        <div className='my-10'>
+          <p className='text-center tracking-widest font-bold text-[#019099] text-sm'> COMPARACIÓN </p>
+          <h1 className='text-center text-[#101d21] text-3xl font-bold tracking-tight font-Montserrat'>Antes y después.</h1>
+        </div>
       <div className='flex justify-center h-auto px-6 w-[85%] md:w-[80%] mx-auto'>
-        <div className='bg-[#f1e3e3] p-5 rounded-xl shadow-soft-blur'>
-          <ReactCompareSlider
+        <div className='bg-white p-5 rounded-xl shadow-soft-blur border border-[#d7e0e1]'>
+          {/* <ReactCompareSlider
             itemOne={<img className='w-full h-full object-cover inset-shadow-md' src={biggieBefore} />}
             itemTwo={<img className='w-full h-full object-cover inset-shadow-md' src={biggieAfter} />}
-          />
+          /> */}
+        <img src={beforeAfter}/>
         </div>
       </div>
     </div>
