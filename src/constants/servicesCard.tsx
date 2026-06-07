220d65c5cc82f77e36bb4aeff892eabccc705da2
wMageblood <enzo.luciano@outlook.es>
2026-06-07 16:38:46 -0600

    fixed some descriptions

--- src/constants/servicesCard.tsx
+++ src/constants/servicesCard.tsx
@@ -1,9 +1,9 @@
 import { Stethoscope, Activity, Bubbles, Bone } from "lucide-react";
 
 const SERVICES_CARD = [
-  { backgroundIcon: <Activity className='absolute text-[#a6e1ca]/25 w-50 h-50 top-2 right-6' />, icon: <Stethoscope />, title: 'Consultas', description: 'Agenda tu consulta para checar el estado salud de tu familiar peludo' },
-  { backgroundIcon: <Bubbles className='absolute text-[#a6e1ca]/25 w-50 h-50 top-0 right-0' />, icon: <Bubbles />, title: 'Estética', description: 'Para ponerlos guapos y elegantes' },
-  { backgroundIcon: <Bone className='absolute text-[#a6e1ca]/25 w-50 h-50 top-0 right-6' />, icon: <Bone />, title: 'Profilaxis Dental', description: 'Asegurate de agender tu limpieza dental para cuidar a nuestros amigos peludos' },
+  { backgroundIcon: <Activity className='absolute text-[#a6e1ca]/25 w-50 h-50 top-2 right-6' />, icon: <Stethoscope />, title: 'Consultas', description: 'Brindamos atención médica personalizada para ayudarte a mantener a tu mascota sana, feliz y acompañada en cada etapa de su vida.' },
+  { backgroundIcon: <Bubbles className='absolute text-[#a6e1ca]/25 w-50 h-50 top-0 right-0' />, icon: <Bubbles />, title: 'Estética', description: 'Cuidamos la imagen y el bienestar de tu mascota con servicios de estética adaptados a sus necesidades.' },
+  { backgroundIcon: <Bone className='absolute text-[#a6e1ca]/25 w-50 h-50 top-0 right-6' />, icon: <Bone />, title: 'Profilaxis Dental', description: 'Ayuda a prevenir enfermedades y cuida la salud de tu mascota con una limpieza dental profesional.' },
 ];
 
 export default SERVICES_CARD;
